90c75d36fde8f4c561cfa9ef733e4d3e9ab3b889
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-02-26 14:01:19 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/SohanRazz/All-in-one-shortner-.git /All-in-one-shortner-
+cd /All-in-one-shortner-
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
